e8a003a3dff5c9c0df921ca5813681c468747aeb
Neel Upadhyaya <neel@bravenewtalent.com>
2013-05-17 20:58:48 +0100

    Simpler server

--- feedophile.js
+++ feedophile.js
@@ -1,12 +1,8 @@
-var http = require('http'),
-    m2node = require('m2node');
+var http = require('http');
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-});
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000, "127.0.0.1");
 
-m2node.run(server, {
-  send_spec: 'tcp://127.0.0.1:9996',
-  recv_spec: 'tcp://127.0.0.1:9997'
-});
+console.log('Server running at http://127.0.0.1:3000/');
